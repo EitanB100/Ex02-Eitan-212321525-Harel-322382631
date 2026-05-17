24272970ed3882803e7bdff34e08d679909d4819
Eitan <goodsky2@gmail.com>
2026-05-17 19:14:53 +0300

    added ctor

--- Ex02/Game.cs
+++ Ex02/Game.cs
@@ -2,9 +2,17 @@
 {
     public class Game
     {
-        GameBoard m_Board;
-        Player[] m_Players;
-        int m_CurrentPlayerIndex;
-        eGameState m_GameState;
+        private GameBoard m_Board;
+        private Player[] m_Players;
+        private int m_CurrentPlayerIndex;
+        private eGameState m_GameState;
+
+        public Game(int i_BoardSize, Player i_Player1, Player i_Player2, int i_StartingPlayerIndex)
+        {
+            m_Board = new GameBoard(i_BoardSize);
+            m_Players[0] = i_Player1;
+            m_Players[1] = i_Player2;
+            m_CurrentPlayerIndex = i_StartingPlayerIndex;
+        }
     }
 }
