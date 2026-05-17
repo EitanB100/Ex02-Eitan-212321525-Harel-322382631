9f2909c3c109eb5bf4a83a76898d27e975345484
Eitan <goodsky2@gmail.com>
2026-05-17 14:37:49 +0300

    added player properties

--- Ex02/Player.cs
+++ Ex02/Player.cs
@@ -1,29 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.AccessControl;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ex02
+﻿namespace Ex02
 {
-    internal class Player
+    public class Player
     {
         private string m_Name = string.Empty;
-        private char m_Symbol = ' ';
+        private ePlayerSymbol m_Symbol = ePlayerSymbol.None;
         private int m_Score = 0;
         private bool m_IsCPU = false;
 
-        public Player(string i_Name, char i_Symbol, bool i_IsCPU)
+        public Player(string i_Name, ePlayerSymbol i_Symbol, bool i_IsCPU)
         {
             m_Name = i_Name;
             m_Symbol = i_Symbol;
             m_IsCPU = i_IsCPU;
         }
 
-        public void AddPoint()
+        public string Name
         {
-            m_Score++;
+            get
+            {
+                return m_Name;
+            }
         }
+
+        public ePlayerSymbol Symbol
+        {
+            get
+            {
+                return m_Symbol;
+            }
+        }
+
+        public int Score
+        {
+            get
+            {
+                return m_Score;
+            }
+            set
+            {
+                m_Score++;
+            }
+        }
+        
+        public bool IsCPU
+        {
+            get
+            {
+                return m_IsCPU;
+            }
+            set
+            {
+                m_IsCPU = value;
+            }
+        }
+
     }
 }
