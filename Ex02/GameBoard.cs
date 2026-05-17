04dd780ab4ba53e1ba5bd0ee5253af5577a1fd39
Eitan <goodsky2@gmail.com>
2026-05-17 15:50:46 +0300

    Added board with fill method and check losing condition method.

---
+++ Ex02/GameBoard.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02
+{
+    public class GameBoard
+    {
+        private readonly int m_BoardSize = 3;
+        private ePlayerSymbol[,] m_Board;
+
+        public GameBoard(int i_BoardSize)
+        {
+            m_BoardSize = i_BoardSize;
+            m_Board = new ePlayerSymbol[i_BoardSize, i_BoardSize];
+
+            fillBoardAtGameInit();
+        }
+
+        private void fillBoardAtGameInit()
+        {
+            for (int i = 0; i < m_BoardSize; i++)
+            {
+                for (int j = 0; j < m_BoardSize; j++)
+                {
+                    m_Board[i, j] = ePlayerSymbol.None;
+                }
+            }
+        }
+
+        public bool checkLosingCondition()
+        {
+            bool isLosingConditionMet = false;
+
+            isLosingConditionMet = (checkRowsForLosingCondition() ||
+                                    checkColumnsForLosingCondition() ||
+                                    checkMainDiagonalForLosingCondition() ||
+                                    checkSecondaryDiagonalForLosingCondition());
+
+            return isLosingConditionMet;
+        }
+
+        private bool checkRowsForLosingCondition()
+        {
+            bool isLosingRowFound = false;
+
+            for (int i = 0; i < m_BoardSize && !isLosingRowFound; i++)
+            {
+                bool isCurrentRowLosingRow = true;
+                ePlayerSymbol firstSymbolInCurrentRow = m_Board[i, 0];
+
+                for (int j = 0; j < m_BoardSize && isCurrentRowLosingRow; j++)
+                {
+                    isCurrentRowLosingRow = ((m_Board[i, j] == ePlayerSymbol.None)
+                       || (m_Board[i, j] != firstSymbolInCurrentRow));
+                }
+
+                isLosingRowFound = isCurrentRowLosingRow;
+            }
+
+            return isLosingRowFound;
+        }
+
+        private bool checkColumnsForLosingCondition()
+        {
+            bool isLosingColumnFound = false;
+
+            for (int i = 0; i < m_BoardSize && !isLosingColumnFound; i++)
+            {
+                bool isCurrentColumnLosingColumn = true;
+                ePlayerSymbol firstSymbolInCurrentColumn = m_Board[0, i];
+
+                for (int j = 0; j < m_BoardSize && isCurrentColumnLosingColumn; j++)
+                {
+                    isCurrentColumnLosingColumn = ((m_Board[j, i] == ePlayerSymbol.None)
+                        || (m_Board[i, j] != firstSymbolInCurrentColumn));
+                }
+
+                isLosingColumnFound = isCurrentColumnLosingColumn;
+            }
+
+            return isLosingColumnFound;
+        }
+
+        private bool checkMainDiagonalForLosingCondition()
+        {
+            bool isDiagonalLosing = false;
+            ePlayerSymbol firstDiagonalSymbol = m_Board[0, 0];
+
+            if (firstDiagonalSymbol != ePlayerSymbol.None)
+            {
+                isDiagonalLosing = true;
+
+                for (int i = 1; i < m_BoardSize && isDiagonalLosing; i++)
+                {
+                    isDiagonalLosing = (firstDiagonalSymbol == m_Board[i,i]);
+                }
+            }
+
+            return isDiagonalLosing;
+        }
+
+        private bool checkSecondaryDiagonalForLosingCondition()
+        {
+            bool isDiagonalLosing = false;
+            ePlayerSymbol firstDiagonalSymbol = m_Board[m_BoardSize - 1, m_BoardSize - 1];
+
+            if (firstDiagonalSymbol != ePlayerSymbol.None)
+            {
+                isDiagonalLosing = true;
+                
+                for (int i = 2; i <= m_BoardSize; i++)
+                {
+                    isDiagonalLosing = (firstDiagonalSymbol == m_Board[m_BoardSize - i, i - 1]);
+                }
+            }
+
+            return isDiagonalLosing;
+        }
+    }
+}
