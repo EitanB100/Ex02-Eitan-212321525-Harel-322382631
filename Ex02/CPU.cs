42027ddd7cf35523042d25aee755dba54b68d40d
Eitan <goodsky2@gmail.com>
2026-05-18 21:38:19 +0300

    Added input error specifics, start of CPU class and CPU implemented in Game manager.

--- Ex02/CPU.cs
+++ Ex02/CPU.cs
@@ -4,6 +4,13 @@ namespace Ex02
 {
     public class CPU 
     {
-        
+        private Random m_CellPicker = new Random();
+
+        public void GetMove(GameBoard i_Board, out int o_Row, out int o_Column)
+        {
+            o_Row = 0;
+            o_Column = 0;
+
+        }
     }
 }
